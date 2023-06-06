e4ec56196fb23f89fb5b6182f8fb197f9adccc11
Salvatore <salvodanna@yahoo.it>
2023-06-06 18:19:50 +0200

    First commit

---
+++ src/co/develhope/introduction/Start.java
@@ -0,0 +1,10 @@
+package co.develhope.introduction;
+
+public class Start {
+    public static void main(String[] args) {
+        String birthday = "11 01 1995";
+        System.out.println("Hello develop java student "+ birthday);
+
+
+    }
+}
